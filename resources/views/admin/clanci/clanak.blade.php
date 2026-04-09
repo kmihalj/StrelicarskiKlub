fc254e98806fbf2a18595305aba44f0bf82241e0
Krešimir Mihalj <kresimir.mihalj@srce.hr>
2026-04-09 09:51:23 +0200

    Dinamičko ažuriranje brojki u članku 40

--- resources/views/admin/clanci/clanak.blade.php
+++ resources/views/admin/clanci/clanak.blade.php
@@ -17,8 +17,15 @@
             @endauth
         </p>
     </div>
+    @php
+        $sadrzajZaPrikaz = (string) $clanak->sadrzaj;
+        if ((int) $clanak->id === \App\Services\Article40DynamicStatsService::ARTICLE_ID) {
+            $sadrzajZaPrikaz = app(\App\Services\Article40DynamicStatsService::class)
+                ->renderArticleContent((int) $clanak->id, $sadrzajZaPrikaz);
+        }
+    @endphp
     <div class="col-lg-12 ck-content">
-        {!! $clanak->sadrzaj !!}
+        {!! $sadrzajZaPrikaz !!}
     </div>
     @php
         $slikeClanka = $clanak->mediji
